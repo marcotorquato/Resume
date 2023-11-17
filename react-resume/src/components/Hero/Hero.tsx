a44b21772fdaab796103e10c9dfd95f4eec4ecc4
Marco Antonio Torquato <63409966+marcotorquato@users.noreply.github.com>
2023-11-17 18:31:16 +0100

    create header component

---
+++ react-resume/src/components/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import './hero.css'
+
+export function Hero() {
+  return (
+    <>
+    <h1>teste</h1>
+    </>
+  )
+
+
+}
---
+++ react-resume/src/components/header/Header.tsx
@@ -0,0 +1,25 @@
+import './header.css'
+import { Icon } from '@iconify/react';
+
+
+export function Header() {
+
+  return (
+    <>
+    <section id="hero" className="d-flex flex-column justify-content-center">
+    <div className="container" data-aos="zoom-in" data-aos-delay="100">
+      <h1>Marco Antonio Torquato</h1>
+      <p>I'm <span className="typed" data-typed-items="Front-End, Developer, Back-End, Developer"></span></p>
+      <div className="social-links">
+        <a href="https://github.com/marcotorquato" className="github" target="_blank"> <Icon icon="mdi:github" />
+        </a>
+        <a href="https://www.instagram.com/marcotorquat/" target="_blank" className="instagram"><Icon icon="mdi:instagram" /></a>
+        <a href="https://www.linkedin.com/in/marcotorquato/"target="_blank" className="linkedin" ><Icon icon="mdi:linkedin" /></a>
+      </div>
+    </div>
+  </section>
+    </>
+  )
+
+
+}

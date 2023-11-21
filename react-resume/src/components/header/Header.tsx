540cc6abcb9cc5b13c72a48ea0fc5d0c291efada
Marco Antonio Torquato <marcotorquato97@gmail.com>
2023-11-21 23:14:29 +0100

    Update Header.tsx

--- react-resume/src/components/header/Header.tsx
+++ react-resume/src/components/header/Header.tsx
@@ -1,22 +1,19 @@
 import './header.css'
-
+import { Icon } from '@iconify/react';
 
 export function Header() {
 
-  
-
   return (
     <>
-    <i className="bi bi-list mobile-nav-toggle d-xl-none"></i>
- 
+    <Icon icon="bi:list"  className='mobile-nav-toggle '/> 
   <header id="header" className="d-flex flex-column justify-content-center">
 
     <nav id="navbar" className="navbar nav-menu">
       <ul>
-        <li><a href="#hero" className="nav-link scrollto active"><i className="bx bx-home"></i> <span>Home</span></a></li>
-        <li><a href="#about" className="nav-link scrollto"><i className="bx bx-user"></i> <span>About</span></a></li>
-        <li><a href="#portfolio" className="nav-link scrollto"><i className="bx bx-book-content"></i> <span>Projects</span></a></li>
-        <li><a href="#contact" className="nav-link scrollto"><i className="bx bx-envelope"></i> <span>Contact</span></a></li>
+        <li><a href="#hero" className="nav-link scrollto active"><Icon icon="ic:outline-home" /> <span>Home</span></a></li>
+        <li><a href="#about" className="nav-link scrollto"><Icon icon="tdesign:user" /><span>About</span></a></li>
+        <li><a href="#portfolio" className="nav-link scrollto"><Icon icon="solar:book-broken" /> <span>Projects</span></a></li>
+        <li><a href="#contact" className="nav-link scrollto"><Icon icon="bi:envelope" /><span>Contact</span></a></li>
       </ul>
     </nav>
 

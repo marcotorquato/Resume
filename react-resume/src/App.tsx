89df05589d5e376c514fa85664835b0cbabceae2
Marco Antonio Torquato <63409966+marcotorquato@users.noreply.github.com>
2023-11-22 11:24:28 +0100

    create about section

--- react-resume/src/App.tsx
+++ react-resume/src/App.tsx
@@ -1,6 +1,7 @@
 import { Header } from './components/header/Header'
 import { Hero } from './components/Hero/Hero'
 import { Footer } from './components/Footer/Footer'
+import { About } from './components/About/About'
 
 import './App.css'
 
@@ -11,7 +12,7 @@ function App() {
     <>
       <Header></Header>
       <Hero></Hero>
-
+      <About></About>
       <Footer></Footer>
   {/* <Preloader></Preloader> */}      
     </>
---
+++ react-resume/src/components/About/About.tsx
@@ -0,0 +1,54 @@
+import './About.css'
+import { Icon } from '@iconify/react';
+import  photoVenere  from '../../assets/venere_profile.png';
+
+
+
+export function About() {
+  
+  return (
+    <>
+    <section id="about" className="about">
+      <div className="container" data-aos="fade-up">
+
+        <div className="section-title">
+          <h2>About</h2>
+          <p>IT Manager, I help small agencies and companies organize to bring their ideas to life. Powered by problems, VS Code and coffee.</p>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-4">
+            <img src={photoVenere} className="img-fluid" alt="photo profile About"/>
+          </div>
+          <div className="col-lg-8 pt-4 pt-lg-0 content">
+            <h3>IT Manager &amp; Developer.</h3>
+            <p className="fst-italic">
+              I started my career helping small businesses with college work, that's what led me to fall in love with helping people with technology. I've always been someone who has a creative and logical side, I recently joined the development niche which vastly expands the way I can add to a company.
+            </p>
+            <div className="row">
+              <div className="col-lg-6">
+                <ul>
+                 
+                  <li><Icon icon="fluent-mdl2:website" /> <strong>Website:</strong> <span>www.marcotorquato.com</span></li>
+                  <li><Icon icon="bi:phone" /> <strong>Phone:</strong> <span>+39 371 5782684</span></li>
+                  <li><Icon icon="carbon:location-filled" /> <strong>City:</strong> <span>Genova, Italy</span></li>
+                </ul>
+              </div>
+              <div className="col-lg-6">
+                <ul>
+                  <li><Icon icon="icon-park-outline:degree-hat" /> <strong>Degree:</strong> <span>Bachelor</span></li>
+                  <li><Icon icon="line-md:email" /> <strong>Email:</strong> <span>contact@marcotorquato.com</span></li>
+                  <li><Icon icon="eos-icons:job" /> <strong>Freelance:</strong> <span>Available</span></li>
+                </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+    </section>
+    </>
+  )
+
+
+}

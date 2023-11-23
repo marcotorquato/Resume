09002e02fd44937863b6ebd98a03eb5bcd95eea1
Marco Antonio Torquato <63409966+marcotorquato@users.noreply.github.com>
2023-11-23 14:30:18 +0100

    create props projects

--- react-resume/src/components/Portfolio/Portfolio.tsx
+++ react-resume/src/components/Portfolio/Portfolio.tsx
@@ -8,24 +8,49 @@ export function Portfolio() {
 
   return (
     <>
-      <Projects
-        category="Web"
-        imageUrl="assets/img/portfolio/lascitiimpolinare.png"
-        galleryLink="assets/img/portfolio/lascitiimpolinare.png"
-        externalLink="https://lasciatiimpollinare.com/"
-      />
-       <Projects
-        category="Web"
-        imageUrl="assets/img/portfolio/lascitiimpolinare.png"
-        galleryLink="assets/img/portfolio/lascitiimpolinare.png"
-        externalLink="https://lasciatiimpollinare.com/"
-      />
-      <Projects
-        category="Web"
-        imageUrl="assets/img/portfolio/lascitiimpolinare.png"
-        galleryLink="assets/img/portfolio/lascitiimpolinare.png"
-        externalLink="https://lasciatiimpollinare.com/"
-      />
+    <section id="portfolio" className="portfolio section-bg">
+      <div className="container" data-aos="fade-up">
+
+        <div className="section-title">
+          <h2>Projects</h2>
+        </div>
+
+        <div className="row">
+          <div className="col-lg-12 d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
+            <ul id="portfolio-flters">
+              <li data-filter="*" className="filter-active">All</li>
+              <li data-filter=".filter-web">Web</li>
+              <li data-filter=".filter-app">App</li>
+              <li data-filter=".filter-pratices">Pratices</li>
+              <li data-filter=".filter-templates">Templates</li>
+            </ul>
+          </div>
+        </div>
+
+        <div className="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
+          <Projects
+          category="Web"
+          imageUrl="assets/img/portfolio/lascitiimpolinare.png"
+          galleryLink="assets/img/portfolio/lascitiimpolinare.png"
+          externalLink="https://lasciatiimpollinare.com/"
+        />
+        <Projects
+          category="Web"
+          imageUrl="assets/img/portfolio/lascitiimpolinare.png"
+          galleryLink="assets/img/portfolio/lascitiimpolinare.png"
+          externalLink="https://lasciatiimpollinare.com/"
+        />
+        <Projects
+          category="Web"
+          imageUrl="assets/img/portfolio/lascitiimpolinare.png"
+          galleryLink="assets/img/portfolio/lascitiimpolinare.png"
+          externalLink="https://lasciatiimpollinare.com/"
+        />
+        </div>
+
+      </div>
+    </section>
+     
     </>
   )
 }

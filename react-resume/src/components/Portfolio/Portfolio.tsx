a028ea4baa2dde0f79ea9e333ff41d14e32896fb
Marco Antonio Torquato <63409966+marcotorquato@users.noreply.github.com>
2023-11-29 16:44:01 +0100

    refacture code

--- react-resume/src/components/Portfolio/Portfolio.tsx
+++ react-resume/src/components/Portfolio/Portfolio.tsx
@@ -2,109 +2,49 @@ import './Portfolio.css'
 import  Kross from '../../assets/portfolio/kross.png'
 
 import { Projects } from './Projects/Projects';
-import { useEffect } from 'react';
-import Isotope from 'isotope-layout';
+
 import AOS from 'aos';
 
 
 export function Portfolio() {
 
-  useEffect(() => {
-    const portfolioContainer = document.querySelector('.portfolio-container') as HTMLElement;
-
-    if (portfolioContainer) {
-      const portfolioIsotope = new Isotope(portfolioContainer, {
-        itemSelector: '.portfolio-item',
-      });
-
-      const portfolioFilters = document.querySelectorAll('#portfolio-flters li') as NodeListOf<HTMLElement>;
-
-      const handleClick = (e: Event) => {
-        e.preventDefault();
-
-        portfolioFilters.forEach((el) => {
-          el.classList.remove('filter-active');
-        });
-
-        if (e.target instanceof HTMLElement) {
-          e.target.classList.add('filter-active');
-
-          portfolioIsotope.arrange({
-            filter: e.target.getAttribute('data-filter') || '',
-          });
-
-          // Refresh AOS after the arrangement is complete
-          AOS.refresh();
-        }
-      };
-
-      portfolioFilters.forEach((filter) => {
-        filter.addEventListener('click', handleClick, true);
-      });
-
-      return () => {
-        // Cleanup event listeners when the component unmounts
-        portfolioFilters.forEach((filter) => {
-          filter.removeEventListener('click', handleClick, true);
-        });
-      };
-    }
-  }, []);
+  // Refresh AOS after the arrangement is complete
+  AOS.refresh();
 
   return (
     <>
-    <section id="portfolio" className="portfolio section-bg">
-      <div className="container" data-aos="fade-up">
+      <section className='instagramSection' id="instagram">
+      <div className="container">
+        <h2>Projects</h2>
+        <p>Todos os nossos clientes são tratados</p>
+      </div>
 
-        <div className="section-title">
-          <h2>Projects</h2>
+      <div className="my-carousel gallery ">
+      <Projects imageSrc={Kross} altText="Project Kross" />
+      <Projects imageSrc={Kross} altText="Project Kross" />
+      <Projects imageSrc={Kross} altText="Project Kross" />
+      <Projects imageSrc={Kross} altText="Project Kross" />
+      <Projects imageSrc={Kross} altText="Project Kross" />
+      </div>
+
+      <div className='looptext'>
+        <div>
+          <h3>• #Royalsburger</h3>
+          <h3>• #Royalsburger</h3>
+          <h3>• #Royalsburger</h3>
+          <h3>• #Royalsburger</h3>
+          <h3>• #Royalsburger</h3>
+          <h3>• #Royalsburger</h3>
+          <h3>• #Royalsburger</h3>
+          <h3>• #Royalsburger</h3>
+          <h3>• #Royalsburger</h3>
+          <h3>• #Royalsburger</h3>
+          <h3>• #Royalsburger</h3>
+          <h3>• #Royalsburger</h3>
+          <h3>• #Royalsburger</h3>
+          <h3>• #Royalsburger</h3>
+          <h3>• #Royalsburger</h3>
         </div>
-
-        <div className="row">
-          <div className="col-lg-12 d-flex justify-content-center" data-aos="fade-up" data-aos-delay="100">
-            <ul id="portfolio-flters">
-              <li data-filter="*" className="filter-active">All</li>
-              <li data-filter=".filter-web">Web</li>
-              <li data-filter=".filter-app">App</li>
-              <li data-filter=".filter-pratices">Pratices</li>
-              <li data-filter=".filter-templates">Templates</li>
-            </ul>
-          </div>
-        </div>
-
-        <div className="row portfolio-container" data-aos="fade-up" data-aos-delay="200">
-          <Projects
-          category="web"
-          imageUrl={Kross}
-          externalLink="https://lasciatiimpollinare.com/"
-        />
-         <Projects
-          category="web"
-          imageUrl={Kross}
-          externalLink="https://lasciatiimpollinare.com/"
-        />
-         <Projects
-          category="app"
-          imageUrl={Kross}
-          externalLink="https://lasciatiimpollinare.com/"
-        />
-         <Projects
-          category="pratices"
-          imageUrl={Kross}
-          externalLink="https://lasciatiimpollinare.com/"
-        /> <Projects
-        category="Web"
-        imageUrl={Kross}
-        externalLink="https://lasciatiimpollinare.com/"
-      />
-       <Projects
-          category="Web"
-          imageUrl={Kross}
-          externalLink="https://lasciatiimpollinare.com/"
-        />
-
-        </div>
-
       </div>
     </section>
      
--- react-resume/src/components/Portfolio/Projects/Projects.tsx
+++ react-resume/src/components/Portfolio/Projects/Projects.tsx
@@ -1,27 +1,22 @@
-import { Icon } from '@iconify/react';
 
-interface PortfolioItemProps {
-  category: string;
-  imageUrl: string;
-  externalLink: string;
+import ScrollCarousel from 'scroll-carousel';
+
+interface MyCarouselProps {
+  imageSrc: string;
+  altText: string;
 }
-export function Projects({category, imageUrl, externalLink }: PortfolioItemProps) {
+
+export function Projects({imageSrc, altText }: MyCarouselProps) {
+
+  new ScrollCarousel(".my-carousel", {smartSpeed: true, direction: "ltr"});
 
   return (
     <>
-     <div className="col-lg-4 col-md-6 portfolio-item filter-web filter-app">
-      <div className="portfolio-wrap">
-        <img src={imageUrl} className="img-fluid" alt="" />
-        <div className="portfolio-info">
-          <p>{category}</p>
-          <div className="portfolio-links">
-            <a href={externalLink} className="portfolio-details-lightbox" data-glightbox="type: external" title="Portfolio Details">
-              <Icon icon="fluent-mdl2:website" />
-            </a>
-          </div>
-        </div>
-      </div>
-    </div>
+
+<div className="my-carousel gallery ">
+  <img className="img-projects" src={imageSrc} alt={altText} /> 
+</div>
+    
     </>
   )
 }

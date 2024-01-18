4f1db0321b6e0c80e26a537d1f7ca40e800802b4
Marco Antonio Torquato <marcotorquato97@gmail.com>
2024-01-18 22:46:18 +0100

    add google anal

--- react-resume/src/main.tsx
+++ react-resume/src/main.tsx
@@ -7,6 +7,13 @@ import { BrowserRouter } from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'aos/dist/aos.css';
 
+import ReactGA from 'react-ga';
+
+// Inicializa o Google Analytics
+ReactGA.initialize('G-L4R42FQEPY');
+
+
+
 ReactDOM.createRoot(document.getElementById('root')!).render(
 
   <BrowserRouter>
